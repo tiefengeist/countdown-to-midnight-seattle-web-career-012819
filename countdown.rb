58e04752a57ba21331feadbebd282f046a6c0066
Sahir Nambiar <sahirnambiar@gmail.com>
2019-01-19 02:41:41 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
-def countdown
+def countdown(ord)
+  while ord > 0
+    puts "#{ord} SECOND(S)!"
+    ord -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+def countdown_with_sleep(ord)
+  while ord > 0
+    puts "#{ord} SECOND(S)!"
+    sleep(1)
+    ord -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
